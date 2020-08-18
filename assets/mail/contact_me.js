c86eb21588d2da305adb4b9464d4cfa1b78fecb5
DaSandez <49734822+DaSandez@users.noreply.github.com>
2020-08-18 00:46:12 -0300

    ahora sí está todo listo

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -21,7 +21,7 @@ $(function () {
             $this = $("#sendMessageButton");
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
             $.ajax({
-                url: "https://correo-portfolio.herokuapp.com",
+                url: "https://correo-portfolio-back.notasalmargen.net",
                 type: "POST",
                 data: {
                     name: name,
