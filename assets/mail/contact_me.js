bd0039d2b393fe6715f0d4bf3ce89a8c4b919f3a
DaSandez <49734822+DaSandez@users.noreply.github.com>
2020-08-24 13:16:45 -0300

    modificando pila de scripts

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -20,19 +20,15 @@ $(function () {
             }
             $this = $("#sendMessageButton");
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-            let urlenconded = `name=${name}&email=${email}&phone=${phone}&message=${message}`;
             $.ajax({
                 url: "https://notasalmargen.net/portfolio-mail-back",
                 type: "POST",
-                data: urlenconded//{
+                data: `name=${name}&email=${email}&phone=${phone}&message=${message}`//{
                     /* name: name,
                     phone: phone,
                     email: email,
                     message: message,
                 } */,
-                headers: {
-                    "Content-Type": "multipart/form-data"
-                },
                 crossDomain: true,
                 cache: false,
                 success: function (res) {
